7886f83ca367543cd6f63177822d69ddba43998c
AidanSawatzky <aidan.d.sawatzky@edu.sait.ca>
2025-09-15 10:26:25 -0600

    Initial Push

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function StudentInfo() {
+  return (
+    <main>
+        <p>Aidan Sawatzky</p>
+        <Link href="https://github.com/AidanSawatzky">Github</Link>
+    </main>
+    );
+}

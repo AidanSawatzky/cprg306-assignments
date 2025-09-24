85799648aaafebdc244f0ae2d7753c12d7651ef8
AidanSawatzky <aidan.d.sawatzky@edu.sait.ca>
2025-09-24 10:29:04 -0600

    Update page.js

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -4,7 +4,7 @@ import ItemList from "./item-list";
 export default function Page() {
 
     return (
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start bg-gray-300 min-h-screen">
+      <main className="flex flex-col gap-[32px] sm:items-center bg-gray-300 min-h-screen">
         <div className="w-full h-12 bg-blue-500">
           <h1 className="text-3xl font-bold text-center">
             Shopping List
